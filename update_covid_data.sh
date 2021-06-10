a59327eb6bbd1812835554ddf4018389e84b00f3
jueves-server <github_jueves-server@martes1.fastmail.fm>
2021-06-10 19:11:39 +0100

    Changed execution path

--- update_covid_data.sh
+++ update_covid_data.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 path="/home/pi/covid_canarias_data/"
 log_file="/home/pi/covid_canarias_data/covid_data.log"
-script="/home/pi/covid_canarias_data/get_covid_daily_data.py"
 date_time="$(date)"
 echo $date_time >>  $log_file
 
 # Get data
-#python3 $script >> $log_file
-python3 /home/pi/covid_canarias_data/get_covid_daily_data.py >> $log_file
+cd $path
+python3 get_covid_daily_data.py >> $log_file
 
 # Upload data
-cd $path
 git add data/cv19_asignacion.csv >> $log_file
 git add data/cv19_asignacion_agrupados.csv >> $log_file
 git add data/cv19_residencia.csv >> $log_file
