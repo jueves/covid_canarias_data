cbe3584061af9c9675ec13131026eacb35354f3e
Luis Cobiella Hernández <alu0100358797@ull.edu.es>
2021-06-12 13:01:56 +0100

    Change aggregated dataset filename

--- update_covid_data.sh
+++ update_covid_data.sh
@@ -10,7 +10,7 @@ python3 get_covid_daily_data.py >> $log_file
 
 # Upload data
 git add data/cv19_asignacion.csv >> $log_file
-git add data/cv19_asignacion_agrupados.csv >> $log_file
+git add data/cv19_asignacion_agrupados_collected.csv >> $log_file
 git add data/cv19_residencia.csv >> $log_file
 
 git commit -m "Data update" >> $log_file
